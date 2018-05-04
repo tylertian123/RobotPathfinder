bd440531fc849b4f00c618e15170c9dbf284c7d2
Tyler Tian <tylertian123@gmail.com>
2018-05-03 23:56:31 -0400

    Add arguments for visualization tool

--- src/robot/pathfinder/tools/TrajectoryVisualizationTool.java
+++ src/robot/pathfinder/tools/TrajectoryVisualizationTool.java
@@ -31,7 +31,14 @@ public class TrajectoryVisualizationTool {
 	static JTextField waypointX = new JTextField();
 	static JTextField waypointY = new JTextField();
 	static JTextField waypointHeading = new JTextField();
-	static JPanel optionsPanel;
+	static JPanel waypointPanel;
+	
+	static JTextField baseWidth = new JTextField();
+	static JTextField alpha = new JTextField();
+	static JTextField segments = new JTextField();
+	static JTextField maxVelocity = new JTextField();
+	static JTextField maxAcceleration = new JTextField();
+	static JPanel argumentsPanel;
 	
 	static ArrayList<Waypoint> waypoints = new ArrayList<Waypoint>();
 	
@@ -68,24 +75,66 @@ public class TrajectoryVisualizationTool {
 		waypointX.setPreferredSize(new Dimension(100, 20));
 		waypointY.setPreferredSize(new Dimension(100, 20));
 		waypointHeading.setPreferredSize(new Dimension(100, 20));
-		optionsPanel = new JPanel();
-		optionsPanel.setLayout(new BoxLayout(optionsPanel, BoxLayout.PAGE_AXIS));
-		optionsPanel.add(new JLabel("Waypoint X"));
-		optionsPanel.add(waypointX);
-		optionsPanel.add(new JLabel("Waypoint Y"));
-		optionsPanel.add(waypointY);
-		optionsPanel.add(new JLabel("Robot Direction (Degrees)"));
-		optionsPanel.add(waypointHeading);
+		waypointPanel = new JPanel();
+		waypointPanel.setLayout(new BoxLayout(waypointPanel, BoxLayout.PAGE_AXIS));
+		waypointPanel.add(new JLabel("Waypoint X"));
+		waypointPanel.add(waypointX);
+		waypointPanel.add(new JLabel("Waypoint Y"));
+		waypointPanel.add(waypointY);
+		waypointPanel.add(new JLabel("Robot Direction (Degrees)"));
+		waypointPanel.add(waypointHeading);
 		
 		mainPanel = new JPanel();
 		mainPanel.setLayout(new BorderLayout());
 		JTable table = new JTable(new WaypointTableModel(COLUMN_NAMES, 0));
 		table.setCellSelectionEnabled(false);
+		table.setRowSelectionAllowed(true);
 		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		JScrollPane scrollPane = new JScrollPane(table);
 		scrollPane.setPreferredSize(new Dimension(480, 300));
 		table.setFillsViewportHeight(true);
 		mainPanel.add(scrollPane, BorderLayout.PAGE_START);
+		
+		argumentsPanel = new JPanel();
+		maxVelocity = new JTextField();
+		maxAcceleration = new JTextField();
+		baseWidth = new JTextField();
+		alpha = new JTextField();
+		segments = new JTextField();
+		Dimension textFieldSize = new Dimension(75, 20);
+		maxVelocity.setPreferredSize(textFieldSize);
+		//maxVelocity.setMaximumSize(textFieldSize);
+		maxAcceleration.setPreferredSize(textFieldSize);
+		//maxAcceleration.setMaximumSize(textFieldSize);
+		baseWidth.setPreferredSize(textFieldSize);
+		//baseWidth.setMaximumSize(textFieldSize);
+		alpha.setPreferredSize(textFieldSize);
+		//alpha.setMaximumSize(textFieldSize);
+		segments.setPreferredSize(textFieldSize);
+		//segments.setMaximumSize(textFieldSize);
+		
+		JPanel subPanel1 = new JPanel();
+		subPanel1.setLayout(new BoxLayout(subPanel1, BoxLayout.PAGE_AXIS));
+		subPanel1.add(new JLabel("Maximum Velocity"));
+		subPanel1.add(maxVelocity);
+		subPanel1.add(new JLabel("Maximum Acceleration"));
+		subPanel1.add(maxAcceleration);
+		JPanel subPanel2 = new JPanel();
+		subPanel2.setLayout(new BoxLayout(subPanel2, BoxLayout.PAGE_AXIS));
+		subPanel2.add(new JLabel("Base Plate Width"));
+		subPanel2.add(baseWidth);
+		subPanel2.add(new JLabel("Alpha"));
+		subPanel2.add(alpha);
+		JPanel subPanel3 = new JPanel();
+		subPanel3.setLayout(new BoxLayout(subPanel3, BoxLayout.PAGE_AXIS));
+		subPanel3.add(new JLabel("Number of Segments"));
+		subPanel3.add(segments);
+		
+		argumentsPanel.add(subPanel1);
+		argumentsPanel.add(subPanel2);
+		argumentsPanel.add(subPanel3);
+		
+		mainPanel.add(argumentsPanel, BorderLayout.CENTER);
 		
 		buttonsPanel = new JPanel();
 		
@@ -97,7 +146,7 @@ public class TrajectoryVisualizationTool {
 				do {
 					int selectedRow = table.getSelectedRow();
 					
-					int response = JOptionPane.showConfirmDialog(mainFrame, optionsPanel, "New Waypoint...", JOptionPane.OK_CANCEL_OPTION);
+					int response = JOptionPane.showConfirmDialog(mainFrame, waypointPanel, "New Waypoint...", JOptionPane.OK_CANCEL_OPTION);
 					if(response != JOptionPane.OK_OPTION)
 						break;
 					try {

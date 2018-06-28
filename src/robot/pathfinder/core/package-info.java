30e896221527f74e085a15a650ac0d328e6e4aa0
Tyler Tian <tylertian123@gmail.com>
2018-06-28 16:20:03 -0400

    Add package-info.java to all packages and reexport JavaDoc and jars

---
+++ src/robot/pathfinder/core/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains some of the essential classes of RobotPathfinder.
+ * <p>
+ * These classes are used frequently for path and trajectory generation and are the core of RobotPathfinder.
+ * </p>
+ */
+package robot.pathfinder.core;
---
+++ src/robot/pathfinder/tools/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains tools for use with RobotPathfinder.
+ * <p>
+ * This includes a Grapher utility class, and a GUI application for visualizing paths.
+ * </p>
+ */
+package robot.pathfinder.tools;

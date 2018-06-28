350b11ac4c26483c5c4ce77a48a6121934e7898a
Tyler Tian <tylertian123@gmail.com>
2018-06-27 23:16:57 -0400

    Document BasicMoment

--- src/robot/pathfinder/core/trajectory/BasicMoment.java
+++ src/robot/pathfinder/core/trajectory/BasicMoment.java
@@ -32,6 +32,7 @@ public class BasicMoment implements Cloneable {
 	 * @param position The desired position
 	 * @param velocity The desired velocity
 	 * @param acceleration The desired acceleration
+	 * @param heading The desired heading; see {@link #getHeading()} and {@link #getFacing()} for more information.
 	 */
 	public BasicMoment(double position, double velocity, double acceleration, double heading) {
 		d = position;
@@ -44,7 +45,8 @@ public class BasicMoment implements Cloneable {
 	 * @param position The desired position
 	 * @param velocity The desired velocity
 	 * @param acceleration The desired acceleration
-	 * @param t The point in time this moment is located
+	 * @param heading The desired heading; see {@link #getHeading()} and {@link #getFacing()} for more information.
+	 * @param t The desired time
 	 */
 	public BasicMoment(double position, double velocity, double acceleration, double heading, double t) {
 		this(position, velocity, acceleration, heading);
@@ -73,8 +75,8 @@ public class BasicMoment implements Cloneable {
 		d = position;
 	}
 	/**
-	 * Retrieves the desired position of the moment.
-	 * @return The desired position of the moment
+	 * Retrieves the position of the moment.
+	 * @return The position of the moment
 	 */
 	public double getPosition() {
 		return d;
@@ -87,33 +89,79 @@ public class BasicMoment implements Cloneable {
 		v = velocity;
 	}
 	/**
-	 * Retrieves the desired velocity of the moment.
-	 * @return The desired velocity
+	 * Retrieves the velocity of the moment.
+	 * @return The velocity
 	 */
 	public double getVelocity() {
 		return v;
 	}
 	/**
-	 * Sets the acceleration of the moment
+	 * Sets the acceleration of the moment.
 	 * @param acceleration The new acceleration
 	 */
 	public void setAcceleration(double acceleration) {
 		a = acceleration;
 	}
 	/**
-	 * Retrieves the desired acceleration of the moment
-	 * @return The desired acceleration of the moment
+	 * Retrieves the acceleration of the moment.
+	 * @return The acceleration of the moment
 	 */
 	public double getAcceleration() {
 		return a;
 	}
 	
+	/**
+	 * Sets the heading of the moment.
+	 * <p>
+	 * Headings are the direction of the velocity vector; that is, <em><b>they're the direction the robot is moving
+	 * in, not the direction the robot is facing</b></em>. For the direction that the robot is <em>facing</em>,
+	 * use {@link #getFacing()} instead.
+	 * </p>
+	 * <p>
+	 * The angles are in radians and follow the unit circle, that is, increasing counterclockwise. <em><b>They're
+	 * relative to the positive x axis, not the initial direction of the robot</b></em>. For example, if the
+	 * first waypoint used to generate a trajectory has a heading of pi/2, then the angle that represents "forwards"
+	 * is also pi/2.
+	 * </p>
+	 * @param heading The new heading
+	 */
 	public void setHeading(double heading) {
 		this.heading = heading;
 	}
+	/**
+	 * Retrieves the heading of the moment. <em>Not to be confused with {@link #getFacing()}.</em>
+	 * <p>
+	 * Headings are the direction of the velocity vector; that is, <em><b>they're the direction the robot is moving
+	 * in, not the direction the robot is facing</b></em>. For the direction that the robot is <em>facing</em>,
+	 * use {@link #getFacing()} instead.
+	 * </p>
+	 * <p>
+	 * The angles are in radians and follow the unit circle, that is, increasing counterclockwise. <em><b>They're
+	 * relative to the positive x axis, not the initial direction of the robot</b></em>. For example, if the
+	 * first waypoint used to generate a trajectory has a heading of pi/2, then the angle that represents "forwards"
+	 * is also pi/2.
+	 * </p>
+	 * @return The heading of the robot at this moment
+	 */
 	public double getHeading() {
 		return heading;
 	}
+	/**
+	 * Retrieves the direction the robot is <em>facing</em> at this moment in time. <em>Not to be confused with
+	 * {@link #getHeading()}.</em>
+	 * <p>
+	 * The angles are in radians and follow the unit circle, that is, increasing counterclockwise. <em><b>They're
+	 * relative to the positive x axis, not the initial direction of the robot</b></em>. For example, if the
+	 * first waypoint used to generate a trajectory has a heading of pi/2, then the angle that represents "forwards"
+	 * is also pi/2.
+	 * </p>
+	 * <p>
+	 * This value is calculated from the heading of the robot and cannot be set directly. It is implemented by
+	 * returning the heading when the velocity is positive, and returning the negative of the heading when the 
+	 * velocity is negative.
+	 * </p>
+	 * @return
+	 */
 	public double getFacing() {
 		return v >= 0 ? heading : -heading;
 	}

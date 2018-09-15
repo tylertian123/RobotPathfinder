0c832cc7f2a5e12096245bca4634eb0caf18e8d1
Tyler Tian <tylertian123@gmail.com>
2018-09-15 17:20:01 -0400

    Document Trajectory classes

--- src/robot/pathfinder/core/trajectory/package-info.java
+++ src/robot/pathfinder/core/trajectory/package-info.java
@@ -4,9 +4,7 @@
  * <p>
  * A trajectory not only defines the points that a robot will path through, it also provides information about the
  * velocity, acceleration and direction. Most importantly, for any given time, it can provide information about
- * the robot's whereabouts and other information. Note that unlike paths and spline segments, there is no common
- * ancestor for all the trajectory types, because different types of trajectories are suited to different types
- * of robots, and thus have very different interfaces.
+ * the robot's whereabouts and other information.
  * </p>
  */
 package robot.pathfinder.core.trajectory;

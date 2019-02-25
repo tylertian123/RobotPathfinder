13cd5de5a2718072523575b8d69e2e6b7799bba2
Tyler Tian <tylertian123@gmail.com>
2019-02-25 13:31:32 -0500

    Add cubicpath.h and improve bezierpath.h

--- src/main/cpp/include/cubicpath.h
+++ src/main/cpp/include/cubicpath.h
@@ -1,31 +1,31 @@
 #pragma once
 
-#include "beziersegment.h"
+#include "cubicsegment.h"
 #include "path.h"
 #include "vec2d.h"
 #include <stdexcept>
 #include <cmath>
 
 namespace rpf {
-    class BezierPath : public rpf::Path {
+    class CubicPath : public Path {
     public:
-        BezierPath(const std::vector<Waypoint> &waypoints, double alpha) {
+        CubicPath(const std::vector<Waypoint> &waypoints, double alpha) {
             this->waypoints = waypoints;
             this->alpha = alpha;
-            type = PathType::BEZIER;
+            type = PathType::CUBIC_HERMITE;
 
             if(waypoints.size() < 2) {
                 throw std::runtime_error("Not enough waypoints");
             }
 
             segments.resize(waypoints.size() - 1);
-            
+
             for(int i = 0; i < waypoints.size() - 1; i ++) {
-                segments.push_back(std::make_unique<BezierSegment>(BezierSegment::from_hermite(
+                segments.push_back(std::make_unique<CubicSegment>(
                     static_cast<Vec2D>(waypoints[i]), static_cast<Vec2D>(waypoints[i + 1]),
                     Vec2D(std::cos(waypoints[i].heading) * alpha, std::sin(waypoints[i].heading) * alpha),
                     Vec2D(std::cos(waypoints[i + 1].heading) * alpha, std::sin(waypoints[i + 1].heading) * alpha)
-                )));
+                ));
             }
         }
     };

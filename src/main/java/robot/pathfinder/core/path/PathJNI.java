b0041f769438a3cbdb09f7297592e5ee6f2b7b2a
Tyler Tian <tylertian123@gmail.com>
2019-02-26 23:30:00 -0500

    Add base PathJNI class

---
+++ src/main/java/robot/pathfinder/core/path/PathJNI.java
@@ -0,0 +1,16 @@
+package robot.pathfinder.core.path;
+
+import robot.pathfinder.core.Waypoint;
+
+public class PathJNI {
+    static {
+        System.loadLibrary("RobotPathfinder");
+    }
+
+    private long _nativePtr;
+
+    private static final int PT_BEZIER = 1;
+    private static final int PT_CUBIC_HERMITE = 2;
+    private static final int PT_QUINTIC_HERMITE = 3;
+    private native void construct(Waypoint[] waypoints, double alpha, int type);
+}

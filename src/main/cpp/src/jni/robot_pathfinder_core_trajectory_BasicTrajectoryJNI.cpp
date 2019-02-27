52b3a9a7dfa0c5394242cada0bf89977b09b604c
Tyler Tian <tylertian123@gmail.com>
2019-02-26 23:25:06 -0500

    Delete BasicTrajectory JNI

--- src/main/cpp/src/jni/robot_pathfinder_core_trajectory_BasicTrajectoryJNI.cpp
+++
@@ -1,8 +0,0 @@
-#include "jni/robot_pathfinder_core_trajectory_BasicTrajectoryJNI.h"
-#include "jniutil.h"
-
-JNIEXPORT void JNICALL Java_robot_pathfinder_core_trajectory_BasicTrajectoryJNI_construct
-        (JNIEnv *env, jobject obj, jdouble max_v, jdouble max_a, jdouble base_width, jobjectArray waypoints, 
-        jdouble alpha, jint seg_count, jboolean is_tank, jint path_type) {
-    
-}

c9135bd343e07d9b1ef4ba4e6d15c16f560b6087
Tyler Tian <tylertian123@gmail.com>
2019-02-27 11:58:51 -0500

    Add rpf::get_field<T>

---
+++ src/main/cpp/include/jni/jniutil.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <jni.h>
+
+namespace rpf {
+    template <typename T>
+    T* get_obj_ptr(JNIEnv *env, jobject obj) {
+        jfieldID fid = env->GetFieldID(env->GetObjectClass(obj), "_nativePtr", "J");
+        return reinterpret_cast<T*>(env->GetLongField(obj, fid));
+    }
+    
+    template <typename T>
+    T get_field(JNIEnv *env, jobject obj, const char *fname);
+    template <>
+    jboolean get_field<jboolean>(JNIEnv *env, jobject obj, const char *fname) {
+        jfieldID fid = env->GetFieldID(env->GetObjectClass(obj), fname, "Z");
+        return env->GetBooleanField(obj, fid);
+    }
+    template <>
+    jbyte get_field<jbyte>(JNIEnv *env, jobject obj, const char *fname) {
+        jfieldID fid = env->GetFieldID(env->GetObjectClass(obj), fname, "B");
+        return env->GetByteField(obj, fid);
+    }
+    template <>
+    jchar get_field<jchar>(JNIEnv *env, jobject obj, const char *fname) {
+        jfieldID fid = env->GetFieldID(env->GetObjectClass(obj), fname, "C");
+        return env->GetCharField(obj, fid);
+    }
+    template <>
+    jshort get_field<jshort>(JNIEnv *env, jobject obj, const char *fname) {
+        jfieldID fid = env->GetFieldID(env->GetObjectClass(obj), fname, "S");
+    }
+    template <>
+    jint get_field<jint>(JNIEnv *env, jobject obj, const char *fname) {
+        jfieldID fid = env->GetFieldID(env->GetObjectClass(obj), fname, "I");
+        return env->GetIntField(obj, fid);
+    }
+    template <>
+    jlong get_field<jlong>(JNIEnv *env, jobject obj, const char *fname) {
+        jfieldID fid = env->GetFieldID(env->GetObjectClass(obj), fname, "J");
+        return env->GetLongField(obj, fid);
+    }
+    template <>
+    jfloat get_field<jfloat>(JNIEnv *env, jobject obj, const char *fname) {
+        jfieldID fid = env->GetFieldID(env->GetObjectClass(obj), fname, "F");
+        return env->GetFloatField(obj, fid);
+    }
+    template <>
+    jdouble get_field<jdouble>(JNIEnv *env, jobject obj, const char *fname) {
+        jfieldID fid = env->GetFieldID(env->GetObjectClass(obj), fname, "D");
+        return env->GetDoubleField(obj, fid);
+    }
+}
---
+++ src/main/cpp/src/jni/robot_pathfinder_core_path_JNIPath.cpp
@@ -0,0 +1,8 @@
+#include "robot_pathfinder_core_path_JNIPath.h"
+#include "paths.h"
+#include "jni/jniutil.h"
+
+JNIEXPORT void JNICALL Java_robot_pathfinder_core_path_JNIPath__1construct
+        (JNIEnv *, jobject, jobjectArray, jdouble, jint) {
+    
+}

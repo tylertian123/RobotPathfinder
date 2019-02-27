1aff055c4075626a36b31f5e22589bbcad50ecdb
Tyler Tian <tylertian123@gmail.com>
2019-02-26 23:33:57 -0500

    Add JNI header for JNIPath and use JNIWaypoint in JNIPath

--- src/main/java/robot/pathfinder/core/path/JNIPath.java
+++ src/main/java/robot/pathfinder/core/path/JNIPath.java
@@ -1,5 +1,6 @@
 package robot.pathfinder.core.path;
 
+import robot.pathfinder.core.JNIWaypoint;
 import robot.pathfinder.core.Waypoint;
 
 public class JNIPath {
@@ -12,5 +13,5 @@ public class JNIPath {
     private static final int PT_BEZIER = 1;
     private static final int PT_CUBIC_HERMITE = 2;
     private static final int PT_QUINTIC_HERMITE = 3;
-    private native void construct(Waypoint[] waypoints, double alpha, int type);
+    private native void construct(JNIWaypoint[] waypoints, double alpha, int type);
 }

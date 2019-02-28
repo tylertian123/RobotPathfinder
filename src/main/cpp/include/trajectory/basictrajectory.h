9bf8d4fb5117a9fc305686aea5c4ec7030874836
Tyler Tian <tylertian123@gmail.com>
2019-02-28 14:18:39 -0500

    Add part of C++ BasicTrajectory

--- src/main/cpp/include/trajectory/basictrajectory.h
+++ src/main/cpp/include/trajectory/basictrajectory.h
@@ -1,7 +1,25 @@
 #pragma once
 
+#include "paths.h"
+#include "trajectory/basicmoment.h"
+#include "robotspecs.h"
+#include "math/vec2d.h"
+#include <memory>
+#include <vector>
+
 namespace rpf {
     class BasicTrajectory {
+    public:
 
+
+    protected:
+        std::unique_ptr<Path> path;
+        std::vector<BasicMoment> moments;
+        std::vector<Vec2D> hvecs;
+        std::vector<double> patht;
+        std::vector<double> pathr;
+        double init_facing;
+
+        RobotSpecs specs;
     };
 }

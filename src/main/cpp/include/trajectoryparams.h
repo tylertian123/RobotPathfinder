091ca46d675731e9c5c0027c7ce3c7bb459b65a9
Tyler Tian <tylertian123@gmail.com>
2019-03-04 22:40:03 -0500

    Add TrajectoryParams for C++

---
+++ src/main/cpp/include/trajectoryparams.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "waypoint.h"
+#include "paths.h"
+#include <vector>
+#include <limits>
+
+namespace rpf {
+    struct TrajectoryParams {
+        std::vector<Waypoint> waypoints;
+        double alpha = std::numeric_limits<double>::quiet_NaN();
+        int seg_count;
+        bool is_tank;
+        PathType type;
+    };
+}

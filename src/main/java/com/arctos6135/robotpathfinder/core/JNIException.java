e6de724868f1bdf9c22a14ea5f43bc7c54cc17e5
Tyler Tian <tylertian123@gmail.com>
2019-03-06 21:49:11 -0500

    Add JNIException class

---
+++ src/main/java/com/arctos6135/robotpathfinder/core/JNIException.java
@@ -0,0 +1,13 @@
+package com.arctos6135.robotpathfinder.core;
+
+public class JNIException extends RuntimeException {
+    
+    private static final long serialVersionUID = 7424682501003868216L;
+
+    public JNIException() {
+        super();
+    }
+    public JNIException(String msg) {
+        super(msg);
+    }
+}

e99ae1b9b1a43744274fa64e5dc4919c0b696ddc
Tyler Tian <tylertian123@gmail.com>
2019-03-10 22:24:47 -0400

    Add Java wrapper for JNIBasicTrajectory constructor

--- src/main/java/com/arctos6135/robotpathfinder/core/trajectory/JNIBasicTrajectory.java
+++ src/main/java/com/arctos6135/robotpathfinder/core/trajectory/JNIBasicTrajectory.java
@@ -4,6 +4,7 @@ import com.arctos6135.robotpathfinder.core.GlobalLibraryLoader;
 import com.arctos6135.robotpathfinder.core.JNIWaypoint;
 import com.arctos6135.robotpathfinder.core.RobotSpecs;
 import com.arctos6135.robotpathfinder.core.TrajectoryParams;
+import com.arctos6135.robotpathfinder.core.WaypointEx;
 import com.arctos6135.robotpathfinder.core.path.Path;
 
 public class JNIBasicTrajectory implements Trajectory {
@@ -18,7 +19,37 @@ public class JNIBasicTrajectory implements Trajectory {
             double alpha, int segmentCount, int type);
 
     public JNIBasicTrajectory(RobotSpecs specs, TrajectoryParams params) {
-        
+        if(Double.isNaN(specs.getMaxVelocity())) {
+            throw new IllegalArgumentException("Max velocity cannot be NaN");
+        }
+        if(Double.isNaN(specs.getMaxAcceleration())) {
+            throw new IllegalArgumentException("Max acceleration cannot be NaN");
+        }
+        if(params.isTank && Double.isNaN(specs.getBaseWidth())) {
+            throw new IllegalArgumentException("Base width cannot be NaN if trajectory is tank drive");
+        }
+        if(params.waypoints == null) {
+            throw new IllegalArgumentException("Waypoints not set");
+        }
+        if(Double.isNaN(params.alpha)) {
+            throw new IllegalArgumentException("Alpha cannot be NaN");
+        }
+        if(params.segmentCount < 1) {
+            throw new IllegalArgumentException("Segment count must be greater than zero");
+        }
+
+        JNIWaypoint[] jniWaypoints = new JNIWaypoint[params.waypoints.length];
+        for(int i = 0; i < params.waypoints.length; i ++) {
+            if(params.waypoints[i] instanceof WaypointEx) {
+                jniWaypoints[i] = new JNIWaypoint(params.waypoints[i].getX(), params.waypoints[i].getY(), params.waypoints[i].getHeading(), ((WaypointEx) params.waypoints[i]).getVelocity());
+            }
+            else {
+                jniWaypoints[i] = new JNIWaypoint(params.waypoints[i].getX(), params.waypoints[i].getY(), params.waypoints[i].getHeading());
+            }
+        }
+
+        _construct(specs.getMaxVelocity(), specs.getMaxAcceleration(), specs.getBaseWidth(), params.isTank, jniWaypoints, 
+                params.alpha, params.segmentCount, params.pathType.getJNIID());
     }
 
     @Override

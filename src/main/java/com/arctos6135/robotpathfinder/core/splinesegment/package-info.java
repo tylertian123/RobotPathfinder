0be7f8702c2c7f1f177737c3a742941dc2a552ea
Tyler Tian <tylertian123@gmail.com>
2019-03-12 12:00:18 -0400

    Remove all old RobotPathfinder classes

--- src/main/java/com/arctos6135/robotpathfinder/core/splinesegment/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This package contains all the different types of spline segments of RobotPathfinder.
- * <p>
- * A spline is a function defined piecewise by polynomials. Splines are what's used to fit the {@link com.arctos6135.robotpathfinder.core.Waypoint Waypoint}s 
- * and construct paths. They're made of many spline segments, and each segment is defined by a parametric curve.
- * </p>
- */
-package com.arctos6135.robotpathfinder.core.splinesegment;

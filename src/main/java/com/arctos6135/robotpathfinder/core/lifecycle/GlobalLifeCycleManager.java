4a1988003674ef27add2a1e0df87e2631cf84b9d
Tyler Tian <tylertian123@gmail.com>
2019-03-13 13:12:05 -0400

    Add docs for GlobalLifecycleManager

--- src/main/java/com/arctos6135/robotpathfinder/core/lifecycle/GlobalLifeCycleManager.java
+++ src/main/java/com/arctos6135/robotpathfinder/core/lifecycle/GlobalLifeCycleManager.java
@@ -4,6 +4,19 @@ import java.lang.ref.ReferenceQueue;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * The {@code GlobalLifeCycleManager} is a singleton that manages the lifecycle of all {@link JNIObject}s 
+ * in RobotPathfinder using {@code PhantomReference}s. This ensures that when an object is being garbage-collected
+ * by the GC, its native counterpart is also freed and no memory leak happens.
+ * 
+ * However, this does not mean that {@link JNIObject#free()} is optional. The GC does not know how much memory each
+ * native object takes up, and thus it will prioritize other large Java objects over {@link JNIObject}s due to its
+ * small size in the JVM, which leads to inefficient memory management.
+ * 
+ * <p>
+ * <b><em>This class is intended for internal use only. Use at your own risk.</em></b>
+ * </p>
+ */
 public final class GlobalLifeCycleManager {
     
     private GlobalLifeCycleManager() {}
@@ -34,16 +47,30 @@ public final class GlobalLifeCycleManager {
         }
     }
 
+    /**
+     * Initializes the {@link GlobalLifeCycleManager}. This will start a daemon thread dedicated to resource
+     * disposal.
+     */
     public static void initialize() {
         resourceDisposalThread = new ResourceDisposalThread();
         resourceDisposalThread.start();
         initialized = true;
     }
     
+    /**
+     * Gets whether or not the {@link GlobalLifeCycleManager} has been initialized.
+     * 
+     * @return Whether the {@link GlobalLifeCycleManager} has been initialized
+     */
     public static boolean isInitialized() {
         return initialized;
     }
 
+    /**
+     * Registers an object to be managed by the {@link GlobalLifeCycleManager}.
+     * 
+     * @param obj The object to be managed
+     */
     public static void register(JNIObject obj) {
         JNIObjectReference ref = new JNIObjectReference(obj, referenceQueue);
         references.add(ref);

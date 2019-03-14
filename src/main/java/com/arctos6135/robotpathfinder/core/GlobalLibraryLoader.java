694752975e18810ebd9d22b51aa925e03bb6c37c
Tyler Tian <tylertian123@gmail.com>
2019-03-14 01:18:41 -0400

    Add docs for GlobalLibraryLoader, JNIException, RobotSpecs and TrajectoryParams

--- src/main/java/com/arctos6135/robotpathfinder/core/GlobalLibraryLoader.java
+++ src/main/java/com/arctos6135/robotpathfinder/core/GlobalLibraryLoader.java
@@ -2,13 +2,34 @@ package com.arctos6135.robotpathfinder.core;
 
 import java.io.File;
 
+/**
+ * The {@code GlobalLibraryLoader} is a static class whose only purpose is to
+ * load the RobotPathfinder shared native library.
+ * 
+ * @author Tyler Tian
+ * @since 3.0.0
+ */
 public final class GlobalLibraryLoader {
-    
-    private GlobalLibraryLoader() {}
+
+    private GlobalLibraryLoader() {
+    }
 
     private static boolean loaded = false;
+
+    /**
+     * Attempts to load the RobotPathfinder shared native library. If the library is
+     * already loaded, this method will have no effect.
+     * <p>
+     * This method will first attempt to load the library by directly invoking
+     * {@code System.loadLibrary()}. If the library is not in the Java library path,
+     * an error message will be printed, and this method will attempt to load the
+     * library from the current directory ("user.dir"). If the library cannot be
+     * found, an error message will be printed, and calls to
+     * {@link #libraryLoaded()} will return {@code false}.
+     * </p>
+     */
     public static void load() {
-        if(loaded) {
+        if (loaded) {
             return;
         }
 
@@ -17,20 +38,28 @@ public final class GlobalLibraryLoader {
             System.loadLibrary("RobotPathfinder");
             loaded = true;
             System.out.println("Library loaded successfully.");
-        }
-        catch(UnsatisfiedLinkError ule) {
-            System.err.println("Warning: RobotPathfinder dynamic library not found in library path. Searching working directory...");
-            System.out.println("Attempting to load " + System.getProperty("user.dir") + File.separator + System.mapLibraryName("RobotPathfinder"));
+        } catch (UnsatisfiedLinkError ule) {
+            System.err.println(
+                    "Warning: RobotPathfinder dynamic library not found in library path. Searching working directory...");
+            System.out.println("Attempting to load " + System.getProperty("user.dir") + File.separator
+                    + System.mapLibraryName("RobotPathfinder"));
             try {
                 System.load(System.getProperty("user.dir") + File.separator + System.mapLibraryName("RobotPathfinder"));
                 loaded = true;
                 System.out.println("Library loaded successfully.");
-            }
-            catch(UnsatisfiedLinkError ule2) {
+            } catch (UnsatisfiedLinkError ule2) {
                 System.err.println("Critical error: Library cannot be loaded.");
             }
         }
     }
+
+    /**
+     * Retrieves whether the RobotPathfinder shared library has been loaded.
+     * <p>
+     * This method will return {@code false} if the {@link #load()} method was never
+     * called, or if the loading failed.
+     * </p>
+     */
     public static boolean libraryLoaded() {
         return loaded;
     }
--- src/main/java/com/arctos6135/robotpathfinder/core/JNIException.java
+++ src/main/java/com/arctos6135/robotpathfinder/core/JNIException.java
@@ -1,12 +1,19 @@
 package com.arctos6135.robotpathfinder.core;
 
+/**
+ * Indicates that something has gone wrong with JNI. It is currently unused.
+ * 
+ * @author Tyler Tian
+ * @since 3.0.0
+ */
 public class JNIException extends RuntimeException {
-    
+
     private static final long serialVersionUID = 7424682501003868216L;
 
     public JNIException() {
         super();
     }
+
     public JNIException(String msg) {
         super(msg);
     }

520c6d185a57570844408953f369c93abd8663c0
Tyler Tian <tylertian123@gmail.com>
2019-04-04 23:03:21 -0400

    Add more basic follower tests

--- src/test/java/com/arctos6135/robotpathfinder/tests/FollowerTest.java
+++ src/test/java/com/arctos6135/robotpathfinder/tests/FollowerTest.java
@@ -1,5 +1,8 @@
 package com.arctos6135.robotpathfinder.tests;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 import com.arctos6135.robotpathfinder.core.RobotSpecs;
 import com.arctos6135.robotpathfinder.core.TrajectoryParams;
 import com.arctos6135.robotpathfinder.core.Waypoint;
@@ -15,9 +18,15 @@ import org.junit.Test;
 
 public class FollowerTest {
 
-    public static Follower.TimestampSource timer = () -> {
-        return System.currentTimeMillis() / 1000.0;
-    };
+    public static class FakeTimer implements Follower.TimestampSource {
+
+        public double value = 0;
+
+        @Override
+        public double getTimestamp() {
+            return value;
+        }
+    }
     public static DistanceSource distancePlaceholder = () -> { return 0d; };
     public static DirectionSource directionPlaceholder = () -> { return 0d; };
     public static Motor motorPlaceholder = (s) -> {};
@@ -38,10 +47,56 @@ public class FollowerTest {
         TankDriveTrajectory traj = new TankDriveTrajectory(specs, params);
 
         Follower follower = new TankDriveFollower(traj, motorPlaceholder, motorPlaceholder, 
-                distancePlaceholder, distancePlaceholder, timer, directionPlaceholder, 0, 0, 0, 0, 0);
+                distancePlaceholder, distancePlaceholder, new FakeTimer(), directionPlaceholder, 0, 0, 0, 0, 0);
         follower.initialize();
         follower.run();
         follower.stop();
+        
+        traj.free();
+    }
+
+    @Test
+    public void testTankDriveFollowerState() {
+        RobotSpecs specs = new RobotSpecs(5, 3, 1);
+        TrajectoryParams params = new TrajectoryParams();
+        params.waypoints = new Waypoint[] {
+            new Waypoint(0, 0, Math.PI / 2),
+            new Waypoint(10, 10, Math.PI / 2),
+            new Waypoint(0, 20, Math.PI),
+        };
+        params.alpha = 20;
+        params.pathType = PathType.QUINTIC_HERMITE;
+        params.sampleCount = 1000;
+
+        TankDriveTrajectory traj = new TankDriveTrajectory(specs, params);
+
+        FakeTimer timer = new FakeTimer();
+        Follower follower = new TankDriveFollower(traj, motorPlaceholder, motorPlaceholder, 
+                distancePlaceholder, distancePlaceholder, timer, directionPlaceholder, 0, 0, 0, 0, 0);
+        // Assertion: Follower is not finished or running initially
+        assertThat(follower.isFinished(), is(false));
+        assertThat(follower.isRunning(), is(false));
+        // Assertion: Follower is not finished and is running after initialization
+        follower.initialize();
+        assertThat(follower.isFinished(), is(false));
+        assertThat(follower.isRunning(), is(true));
+        // Assertion: Follower is not finished and is running after a single iteration
+        follower.run();
+        assertThat(follower.isFinished(), is(false));
+        assertThat(follower.isRunning(), is(true));
+        // Assertion: Follower is finished and is not running after the trajectory finishes
+        timer.value = traj.totalTime() + 1;
+        follower.run();
+        assertThat(follower.isFinished(), is(true));
+        assertThat(follower.isRunning(), is(false));
+        // Assertion: Follower does not reinitialize after being stopped
+        follower.run();
+        assertThat(follower.isFinished(), is(true));
+        assertThat(follower.isRunning(), is(false));
+        // Assertion: Follower is finished and is not running after being stopped
+        follower.stop();
+        assertThat(follower.isFinished(), is(true));
+        assertThat(follower.isRunning(), is(false));
         
         traj.free();
     }

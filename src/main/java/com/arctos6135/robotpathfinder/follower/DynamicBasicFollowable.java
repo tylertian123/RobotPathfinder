6eb51ad5b35613769241d4a0c32a05fc6792918b
Tyler Tian <tylertian123@gmail.com>
2019-05-06 22:43:23 -0400

    Add new interfaces
    Add new interfaces DynamicTankDriveFollowable extends
    TankDriveFollowable, DynamicFollowable and DynamicBasicFollowable
    extends BasicFollowable, DynamicFollowable.

---
+++ src/main/java/com/arctos6135/robotpathfinder/follower/DynamicBasicFollowable.java
@@ -0,0 +1,4 @@
+package com.arctos6135.robotpathfinder.follower;
+
+public interface DynamicBasicFollowable extends BasicFollowable, DynamicFollowable {
+}
---
+++ src/main/java/com/arctos6135/robotpathfinder/follower/DynamicTankDriveFollowable.java
@@ -0,0 +1,4 @@
+package com.arctos6135.robotpathfinder.follower;
+
+public interface DynamicTankDriveFollowable extends TankDriveFollowable, DynamicFollowable {
+}

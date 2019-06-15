1ff60926b45ba87513284cc9f8678097e469a3be
Tyler Tian <tylertian123@gmail.com>
2019-06-15 17:29:13 -0400

    Add replicate last test functionality to TestHelper
    Add ability to make TestHelper read an existing log file and use it to
    replicate the behavior of the last test.

--- src/test/java/com/arctos6135/robotpathfinder/tests/TestHelper.java
+++ src/test/java/com/arctos6135/robotpathfinder/tests/TestHelper.java
@@ -87,17 +87,112 @@ public final class TestHelper {
         }
     }
 
+    // Gets the method name of the caller of the method calling this method.
+    private String getCallerName() {
+        // Take the 4th element of the stack trace
+        // 0 - getStackTrace()
+        // 1 - getCallerName()
+        // 2 - method calling getCallerName()
+        // 3 - method calling the calling method
+        return Thread.currentThread().getStackTrace()[3].getMethodName();
+    }
+
     private Random rand = new Random();
+
+    private boolean replicateLastTest = false;
+
+    /**
+     * Retrieves whether this {@link TestHelper} instance is configured to replicate
+     * the last test.
+     * 
+     * When a {@link TestHelper} instance is configured to replicate the last test,
+     * it reads a log file that it has written previously and extracts all the
+     * generated values from that file. All subsequent calls to
+     * {@link #getDouble(String, double, double)}, {@link #getInt(String, int, int)}
+     * and their variants will now return the logged value instead of a randomly
+     * generated one. This essentially replicates the behavior of the logged test,
+     * hence the name.
+     * 
+     * @return Whether to replicate the last test
+     */
+    public boolean getReplicateLastTest() {
+        return replicateLastTest;
+    }
+
+    /**
+     * Sets whether this {@link TestHelper} instance is configured to replicate the
+     * last test. The name of the log file to read is automatically inferred from
+     * the class name.
+     * 
+     * When a {@link TestHelper} instance is configured to replicate the last test,
+     * it reads a log file that it has written previously and extracts all the
+     * generated values from that file. All subsequent calls to
+     * {@link #getDouble(String, double, double)}, {@link #getInt(String, int, int)}
+     * and their variants will now return the logged value instead of a randomly
+     * generated one. This essentially replicates the behavior of the logged test,
+     * hence the name.
+     * 
+     * @param replicateLastTest Whether to replicate the last test
+     * @throws IOException           If something goes wrong when reading the log
+     *                               file
+     * @throws IllegalStateException If the log file does not exist or is invalid
+     */
+    public void setReplicateLastTest(boolean replicateLastTest) throws IOException {
+        this.replicateLastTest = replicateLastTest;
+        loadLoggedValues(logFile);
+    }
+
+    /**
+     * Sets whether this {@link TestHelper} instance is configured to replicate the
+     * last test.
+     * 
+     * When a {@link TestHelper} instance is configured to replicate the last test,
+     * it reads a log file that it has written previously and extracts all the
+     * generated values from that file. All subsequent calls to
+     * {@link #getDouble(String, double, double)}, {@link #getInt(String, int, int)}
+     * and their variants will now return the logged value instead of a randomly
+     * generated one. This essentially replicates the behavior of the logged test,
+     * hence the name.
+     * 
+     * @param replicateLastTest Whether to replicate the last test
+     * @param logFile           The log file to read and extract values from
+     * @throws IOException           If something goes wrong when reading the log
+     *                               file
+     * @throws IllegalStateException If the log file does not exist or is invalid
+     */
+    public void setReplicateLastTest(boolean replicateLastTest, File logFile) throws IOException {
+        this.replicateLastTest = replicateLastTest;
+        loadLoggedValues(logFile);
+    }
 
     /**
      * Generates a random {@code double} used for testing. Equivalent to
      * {@code getDouble(name, 0, max)}.
      * 
+     * If configured to replicate the last test, this method will return the same
+     * value as written in the log file.
+     * 
      * @param name The name of this {@code double}
      * @param max  The maximum value
      * @return A randomly generated value
+     * @throws IllegalArgumentException If configured to replicate the last test,
+     *                                  but the log file does not contain an entry
+     *                                  for the calling method or name
+     * @see #setReplicateLastTest(boolean, File)
      */
     public double getDouble(String name, double max) {
+        if (replicateLastTest) {
+            String callerName = getCallerName();
+            if (!loggedValues.containsKey(callerName)) {
+                throw new IllegalArgumentException("Loaded log file does not contain entry for method: " + callerName);
+            }
+            if (!loggedValues.get(callerName).doubles.containsKey(name)) {
+                throw new IllegalArgumentException(
+                        "Loaded log file does not contain entry for value with name: " + name);
+            }
+            return loggedValues.get(callerName).doubles.get(name);
+        }
+
         // Generate the double value
         double val = rand.nextDouble() * max;
         // Log it
@@ -111,12 +206,31 @@ public final class TestHelper {
      * {@code double} that can take on any value between {@code min} and
      * {@code max}, inclusive.
      * 
+     * If configured to replicate the last test, this method will return the same
+     * value as written in the log file.
+     * 
      * @param name The name of this {@code double}
      * @param max  The maximum value
      * @param min  The minimum value
      * @return A randomly generated value.
+     * @throws IllegalArgumentException If configured to replicate the last test,
+     *                                  but the log file does not contain an entry
+     *                                  for the calling method or name
+     * @see #setReplicateLastTest(boolean, File)
      */
     public double getDouble(String name, double min, double max) {
+        if (replicateLastTest) {
+            String callerName = getCallerName();
+            if (!loggedValues.containsKey(callerName)) {
+                throw new IllegalArgumentException("Loaded log file does not contain entry for method: " + callerName);
+            }
+            if (!loggedValues.get(callerName).doubles.containsKey(name)) {
+                throw new IllegalArgumentException(
+                        "Loaded log file does not contain entry for value with name: " + name);
+            }
+            return loggedValues.get(callerName).doubles.get(name);
+        }
+
         // Generate the double value
         double val = rand.nextDouble() * (max - min) + min;
         // Log it
@@ -129,11 +243,30 @@ public final class TestHelper {
      * Generates a random {@code int} used for testing. Equivalent to
      * {@code getInt(name, 0, max)}.
      * 
+     * If configured to replicate the last test, this method will return the same
+     * value as written in the log file.
+     * 
      * @param name The name of this {@code int}
      * @param max  The maximum value
      * @return A randomly generated value
+     * @throws IllegalArgumentException If configured to replicate the last test,
+     *                                  but the log file does not contain an entry
+     *                                  for the calling method or name
+     * @see #setReplicateLastTest(boolean, File)
      */
     public int getInt(String name, int max) {
+        if (replicateLastTest) {
+            String callerName = getCallerName();
+            if (!loggedValues.containsKey(callerName)) {
+                throw new IllegalArgumentException("Loaded log file does not contain entry for method: " + callerName);
+            }
+            if (!loggedValues.get(callerName).ints.containsKey(name)) {
+                throw new IllegalArgumentException(
+                        "Loaded log file does not contain entry for value with name: " + name);
+            }
+            return loggedValues.get(callerName).ints.get(name);
+        }
+
         // Generate the int value
         int val = rand.nextInt(max);
         // Log it
@@ -147,12 +280,31 @@ public final class TestHelper {
      * {@code int} that can take on any value between {@code min} (inclusive) and
      * {@code max} (exclusive).
      * 
+     * If configured to replicate the last test, this method will return the same
+     * value as written in the log file.
+     * 
      * @param name The name of this {@code int}
      * @param max  The maximum value
      * @param min  The minimum value
      * @return A randomly generated value.
+     * @throws IllegalArgumentException If configured to replicate the last test,
+     *                                  but the log file does not contain an entry
+     *                                  for the calling method or name
+     * @see #setReplicateLastTest(boolean, File)
      */
     public int getInt(String name, int min, int max) {
+        if (replicateLastTest) {
+            String callerName = getCallerName();
+            if (!loggedValues.containsKey(callerName)) {
+                throw new IllegalArgumentException("Loaded log file does not contain entry for method: " + callerName);
+            }
+            if (!loggedValues.get(callerName).ints.containsKey(name)) {
+                throw new IllegalArgumentException(
+                        "Loaded log file does not contain entry for value with name: " + name);
+            }
+            return loggedValues.get(callerName).ints.get(name);
+        }
+
         // Generate the int value
         int val = rand.nextInt(max - min) + min;
         // Log it
@@ -188,6 +340,10 @@ public final class TestHelper {
      */
     public static void flushAll() throws IOException {
         for (var entry : instances.entrySet()) {
+            // Don't overwrite the log file if replicateLastTest is true
+            if (entry.getValue().replicateLastTest) {
+                continue;
+            }
             File logFile = entry.getValue().logFile;
             // Create file and all dirs
             File parent = logFile.getParentFile();
@@ -240,22 +396,21 @@ public final class TestHelper {
         String line;
         while ((line = reader.readLine()) != null) {
             // Skip empty lines
-            if(line.length() == 0) {
+            if (line.length() == 0) {
                 continue;
             }
             // If line does not start with a label and ends with a colon, it's the start of
             // a new method
-            if(line.charAt(0) != '[' && line.charAt(line.length() - 1) == ':') {
+            if (line.charAt(0) != '[' && line.charAt(line.length() - 1) == ':') {
                 // Get the method name
                 String methodName = line.substring(0, line.length() - 1);
                 // Create a new values object and add it to the map
                 values = new GeneratedValues();
                 loggedValues.put(methodName, values);
-            }
-            else if(line.charAt(0) == '[') {
+            } else if (line.charAt(0) == '[') {
                 try {
                     // If there was no method name, throw exception
-                    if(values == null) {
+                    if (values == null) {
                         reader.close();
                         throw new IllegalStateException("Invalid log file: " + logFile);
                     }
@@ -263,7 +418,7 @@ public final class TestHelper {
                     int labelEnd = line.indexOf(']');
                     String label = line.substring(1, labelEnd);
                     // If it's not a value, skip
-                    if(!label.startsWith("VALUE")) {
+                    if (!label.startsWith("VALUE")) {
                         continue;
                     }
 
@@ -275,29 +430,27 @@ public final class TestHelper {
                     String valueStr = s[1].substring(1);
 
                     // Get value type from label
-                    switch(label) {
-                    case "VALUE.DOUBLE":
-                    {
+                    switch (label) {
+                    case "VALUE.DOUBLE": {
                         double value = Double.parseDouble(valueStr);
                         values.doubles.put(name, value);
                         break;
                     }
-                    case "VALUE.INT":
-                    {
+                    case "VALUE.INT": {
                         int value = Integer.parseInt(valueStr);
                         values.ints.put(name, value);
                         break;
                     }
-                    default: break;
+                    default:
+                        break;
                     }
                 }
                 // These exceptions mean that the log file is not valid
-                catch(StringIndexOutOfBoundsException | NumberFormatException e) {
+                catch (StringIndexOutOfBoundsException | NumberFormatException e) {
                     reader.close();
                     throw new IllegalStateException("Invalid log file: " + logFile);
                 }
-            }
-            else {
+            } else {
                 reader.close();
                 throw new IllegalStateException("Invalid log file: " + logFile);
             }

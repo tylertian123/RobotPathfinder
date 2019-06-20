tylertian123/RobotPathfinder

commit 3bc00c0b7b3f648b0016c7e664fcee5be22670c7

Add getters and setters for an integral term in followers
Add getters and setters for an additional integral term kI in the
followers. Also add missing getters for all other gains.

src/main/java/com/arctos6135/robotpathfinder/follower/DynamicTankDriveFollower.java
@@ -103,13 +103,15 @@ public class DynamicTankDriveFollower extends DynamicFollower<TankDriveMoment> {
 	}
 
 	/**
-	 * Sets the directional-proportional gain of the feedback loop. The
-	 * directional-proportional gain allows the robot to better follow the
+	 * Sets the directional-proportional gain of the feedback loop.
+	 * <p>
+	 * The directional-proportional gain allows the robot to better follow the
 	 * trajectory by trying to follow not just the position, velocity and
 	 * acceleration, but the direction as well. The actual angle the robot is facing
 	 * at a given time is subtracted from the angle it is supposed to be facing, and
 	 * then multiplied by the directional-proportional gain and added/subtracted to
 	 * the outputs of the left and right wheels.
+	 * </p>
 	 * 
 	 * @param kDP The new directional-proportional gain
 	 */
@@ -118,13 +120,15 @@ public class DynamicTankDriveFollower extends DynamicFollower<TankDriveMoment> {
 	}
 
 	/**
-	 * Gets the directional-proportional gain of the feedback loop. The
-	 * directional-proportional gain allows the robot to better follow the
+	 * Gets the directional-proportional gain of the feedback loop.
+	 * <p>
+	 * The directional-proportional gain allows the robot to better follow the
 	 * trajectory by trying to follow not just the position, velocity and
 	 * acceleration, but the direction as well. The actual angle the robot is facing
 	 * at a given time is subtracted from the angle it is supposed to be facing, and
 	 * then multiplied by the directional-proportional gain and added/subtracted to
 	 * the outputs of the left and right wheels.
+	 * </p>
 	 * 
 	 * @return The directional-proportional gain
 	 */
@@ -138,11 +142,12 @@ public class DynamicTankDriveFollower extends DynamicFollower<TankDriveMoment> {
 	 * @param kV  The velocity feedforward
 	 * @param kA  The acceleration feedforward
 	 * @param kP  The proportional gain
+	 * @param kI  The integral gain
 	 * @param kD  The derivative gain
 	 * @param kDP The directional-proportional gain
 	 */
-	public void setGains(double kV, double kA, double kP, double kD, double kDP) {
-		setGains(kV, kA, kP, kD);
+	public void setGains(double kV, double kA, double kP, double kI, double kD, double kDP) {
+		setGains(kV, kA, kP, kI, kD);
 		setDP(kDP);
 	}
 

src/main/java/com/arctos6135/robotpathfinder/follower/TankDriveFollower.java
@@ -30,7 +30,7 @@ public class TankDriveFollower extends Follower<TankDriveMoment> {
 	// Keep track of the error and timestamp of the last iteration to calculate the
 	// derivative
 	protected double initTime, lastTime, lLastErr, rLastErr, lInitDist, rInitDist, initDirection;
-	
+
 	protected double leftErr, rightErr, dirErr;
 	// Store these as member variables so they can be accessed from outside the
 	// class for testing purposes
@@ -51,8 +51,8 @@ public class TankDriveFollower extends Follower<TankDriveMoment> {
 	 * @param kV     The velocity feedforward
 	 * @param kA     The acceleration feedforward
 	 */
-	public TankDriveFollower(Followable<TankDriveMoment> target, Motor lMotor, Motor rMotor, TimestampSource timer, double kV,
-			double kA) {
+	public TankDriveFollower(Followable<TankDriveMoment> target, Motor lMotor, Motor rMotor, TimestampSource timer,
+			double kV, double kA) {
 		setGains(kV, kA, 0, 0, 0);
 		this.target = target;
 		this.lMotor = lMotor;
@@ -167,13 +167,15 @@ public class TankDriveFollower extends Follower<TankDriveMoment> {
 	}
 
 	/**
-	 * Sets the directional-proportional gain of the feedback loop. The
-	 * directional-proportional gain allows the robot to better follow the
+	 * Sets the directional-proportional gain of the feedback loop.
+	 * <p>
+	 * The directional-proportional gain allows the robot to better follow the
 	 * trajectory by trying to follow not just the position, velocity and
 	 * acceleration, but the direction as well. The actual angle the robot is facing
 	 * at a given time is subtracted from the angle it is supposed to be facing, and
 	 * then multiplied by the directional-proportional gain and added/subtracted to
 	 * the outputs of the left and right wheels.
+	 * </p>
 	 * 
 	 * @param kDP The new directional-proportional gain
 	 */
@@ -182,13 +184,15 @@ public class TankDriveFollower extends Follower<TankDriveMoment> {
 	}
 
 	/**
-	 * Gets the directional-proportional gain of the feedback loop. The
-	 * directional-proportional gain allows the robot to better follow the
+	 * Gets the directional-proportional gain of the feedback loop.
+	 * <p>
+	 * The directional-proportional gain allows the robot to better follow the
 	 * trajectory by trying to follow not just the position, velocity and
 	 * acceleration, but the direction as well. The actual angle the robot is facing
 	 * at a given time is subtracted from the angle it is supposed to be facing, and
 	 * then multiplied by the directional-proportional gain and added/subtracted to
 	 * the outputs of the left and right wheels.
+	 * </p>
 	 * 
 	 * @return The directional-proportional gain
 	 */
@@ -202,11 +206,12 @@ public class TankDriveFollower extends Follower<TankDriveMoment> {
 	 * @param kV  The velocity feedforward
 	 * @param kA  The acceleration feedforward
 	 * @param kP  The proportional gain
+	 * @param kI  The integral gain
 	 * @param kD  The derivative gain
 	 * @param kDP The directional-proportional gain
 	 */
-	public void setGains(double kV, double kA, double kP, double kD, double kDP) {
-		setGains(kV, kA, kP, kD);
+	public void setGains(double kV, double kA, double kP, double kI, double kD, double kDP) {
+		setGains(kV, kA, kP, kI, kD);
 		setDP(kDP);
 	}
 

src/main/java/com/arctos6135/robotpathfinder/follower/Follower.java
@@ -11,8 +11,9 @@ import com.arctos6135.robotpathfinder.core.trajectory.Moment;
  * </p>
  * <p>
  * They do so by using both feedback and feedforward control, with a system that
- * consists of 4 parts: proportional and derivative gains on the position error,
- * and velocity and acceleration feedforward (sometimes known as PDVA control).
+ * consists of 5 parts: proportional, integral and derivative gains on the
+ * position error, and velocity and acceleration feedforward (sometimes known as
+ * PIDVA control).
  * </p>
  * <p>
  * To use a follower, one first must provide values for the aforementioned
@@ -30,7 +31,7 @@ import com.arctos6135.robotpathfinder.core.trajectory.Moment;
  */
 abstract public class Follower<T extends Moment> {
 
-	protected double kA, kV, kP, kD;
+	protected double kA, kV, kP, kI, kD;
 	protected Followable<T> target;
 	protected TimestampSource timer;
 
@@ -171,34 +172,55 @@ abstract public class Follower<T extends Moment> {
 	/**
 	 * Sets the gains of the feedback control loop.
 	 * 
-	 * @param kV The velocity feedforward
-	 * @param kA The acceleration feedforward
+	 * @param kV The velocity feedforward coefficient
+	 * @param kA The acceleration feedforward coefficient
 	 * @param kP The proportional gain
+	 * @param kI the integral gain
 	 * @param kD The derivative gain
 	 */
-	public void setGains(double kV, double kA, double kP, double kD) {
+	public void setGains(double kV, double kA, double kP, double kI, double kD) {
 		this.kV = kV;
 		this.kA = kA;
 		this.kP = kP;
+		this.kI = kI;
 		this.kD = kD;
 	}
 
 	/**
-	 * Sets the acceleration feedforward term of the control loop.
+	 * Sets the acceleration feedforward term's coefficient of the control loop.
 	 * 
-	 * @param a The acceleration feedforward
+	 * @param a The acceleration feedforward term's coefficient
 	 */
 	public void setA(double a) {
 		kA = a;
 	}
 
 	/**
-	 * Sets the velocity feedforward term of the control loop.
+	 * Retrieves the acceleration feedforward term's coefficient of the control
+	 * loop.
 	 * 
-	 * @param v The velocity feedforward
+	 * @return The acceleration feedforward term coefficient
+	 */
+	public double getA() {
+		return kA;
+	}
+
+	/**
+	 * Sets the velocity feedforward term's coefficient of the control loop.
+	 * 
+	 * @param v The velocity feedforward term's coefficient
 	 */
 	public void setV(double v) {
 		kV = v;
+	}
+
+	/**
+	 * Retrieves the velocity feedforward term's coefficient of the control loop.
+	 * 
+	 * @return The velocity feedforward term coefficient
+	 */
+	public double getV() {
+		return kV;
 	}
 
 	/**
@@ -211,12 +233,48 @@ abstract public class Follower<T extends Moment> {
 	}
 
 	/**
+	 * Retrieves the proportional gain of the control loop.
+	 * 
+	 * @return The proportional gain
+	 */
+	public double getP() {
+		return kP;
+	}
+
+	/**
+	 * Sets the integral gain of the control loop.
+	 * 
+	 * @param i The integral gain
+	 */
+	public void setI(double i) {
+		kI = i;
+	}
+
+	/**
+	 * Retrieves the integral gain of the control loop.
+	 * 
+	 * @return The integral gain
+	 */
+	public double getI() {
+		return kI;
+	}
+
+	/**
 	 * Sets the derivative gain of the control loop.
 	 * 
 	 * @param d The derivative gain
 	 */
 	public void setD(double d) {
 		kD = d;
+	}
+
+	/**
+	 * Retrieves the derivative gain of the control loop.
+	 * 
+	 * @return The derivative gain
+	 */
+	public double getD() {
+		return kD;
 	}
 
 	/**

commit 1095917b98e3910a5c33bcbb6d0858828b883cc2

Fix bug caused by commit 3bc00c0
Fix bug caused by commit 3bc00c0 and following commits which added an
integral term and changed the signature of Follower.setGains(). The
4-argument and 5-argument overloads were changed to 5-argument and
6-argument overloads, but the existing calls to the old 5-argument
overload, which should have been updated to a 6-argument call, weren't
updated in the constructors of TankDriveFollower and
DynamicTankDriveFollower, and FollowerTest and DynamicFollowerTest.

src/main/java/com/arctos6135/robotpathfinder/follower/DynamicTankDriveFollower.java
@@ -44,8 +44,8 @@ public class DynamicTankDriveFollower extends DynamicFollower<TankDriveMoment> {
 
 	public DynamicTankDriveFollower(DynamicFollowable<TankDriveMoment> target, Motor lMotor, Motor rMotor,
 			AdvancedPositionSource lDistSrc, AdvancedPositionSource rDistSrc, TimestampSource timer, double kV,
-			double kA, double kP, double kD, double updateDelay) {
-		setGains(kV, kA, kP, kD, 0);
+			double kA, double kP, double kI, double kD, double updateDelay) {
+		setGains(kV, kA, kP, kI, kD, 0);
 		this.target = target;
 		this.lMotor = lMotor;
 		this.rMotor = rMotor;
@@ -59,8 +59,9 @@ public class DynamicTankDriveFollower extends DynamicFollower<TankDriveMoment> {
 
 	public DynamicTankDriveFollower(DynamicFollowable<TankDriveMoment> target, Motor lMotor, Motor rMotor,
 			AdvancedPositionSource lDistSrc, AdvancedPositionSource rDistSrc, TimestampSource timer,
-			DirectionSource dirSrc, double kV, double kA, double kP, double kD, double kDP, double updateDelay) {
-		setGains(kV, kA, kP, kD, kDP);
+			DirectionSource dirSrc, double kV, double kA, double kP, double kI, double kD, double kDP,
+			double updateDelay) {
+		setGains(kV, kA, kP, kI, kD, kDP);
 		this.target = target;
 		this.lMotor = lMotor;
 		this.rMotor = rMotor;
@@ -74,8 +75,8 @@ public class DynamicTankDriveFollower extends DynamicFollower<TankDriveMoment> {
 
 	public DynamicTankDriveFollower(DynamicFollowable<TankDriveMoment> target, Motor lMotor, Motor rMotor,
 			PositionSource lDistSrc, PositionSource rDistSrc, TimestampSource timer, double kV, double kA, double kP,
-			double kD, double updateDelay) {
-		setGains(kV, kA, kP, kD, 0);
+			double kI, double kD, double updateDelay) {
+		setGains(kV, kA, kP, kI, kD, 0);
 		this.target = target;
 		this.lMotor = lMotor;
 		this.rMotor = rMotor;
@@ -89,8 +90,8 @@ public class DynamicTankDriveFollower extends DynamicFollower<TankDriveMoment> {
 
 	public DynamicTankDriveFollower(DynamicFollowable<TankDriveMoment> target, Motor lMotor, Motor rMotor,
 			PositionSource lDistSrc, PositionSource rDistSrc, TimestampSource timer, DirectionSource dirSrc, double kV,
-			double kA, double kP, double kD, double kDP, double updateDelay) {
-		setGains(kV, kA, kP, kD, kDP);
+			double kA, double kP, double kI, double kD, double kDP, double updateDelay) {
+		setGains(kV, kA, kP, kI, kD, kDP);
 		this.target = target;
 		this.lMotor = lMotor;
 		this.rMotor = rMotor;

src/main/java/com/arctos6135/robotpathfinder/follower/TankDriveFollower.java
@@ -54,7 +54,7 @@ public class TankDriveFollower extends Follower<TankDriveMoment> {
 	 */
 	public TankDriveFollower(Followable<TankDriveMoment> target, Motor lMotor, Motor rMotor, TimestampSource timer,
 			double kV, double kA) {
-		setGains(kV, kA, 0, 0, 0);
+		setGains(kV, kA, 0, 0, 0, 0);
 		this.target = target;
 		this.lMotor = lMotor;
 		this.rMotor = rMotor;
@@ -84,11 +84,12 @@ public class TankDriveFollower extends Follower<TankDriveMoment> {
 	 * @param kV       The velocity feedforward
 	 * @param kA       The acceleration feedforward
 	 * @param kP       The proportional gain
+	 * @param kI       The integral gain
 	 * @param kD       The derivative gain
 	 */
 	public TankDriveFollower(Followable<TankDriveMoment> target, Motor lMotor, Motor rMotor, PositionSource lDistSrc,
-			PositionSource rDistSrc, TimestampSource timer, double kV, double kA, double kP, double kD) {
-		setGains(kV, kA, kP, kD, 0);
+			PositionSource rDistSrc, TimestampSource timer, double kV, double kA, double kP, double kI, double kD) {
+		setGains(kV, kA, kP, kI, kD, 0);
 		this.target = target;
 		this.lMotor = lMotor;
 		this.rMotor = rMotor;
@@ -119,7 +120,7 @@ public class TankDriveFollower extends Follower<TankDriveMoment> {
 	 */
 	public TankDriveFollower(Followable<TankDriveMoment> target, Motor lMotor, Motor rMotor, TimestampSource timer,
 			DirectionSource dirSrc, double kV, double kA, double kDP) {
-		setGains(kV, kA, 0, 0, kDP);
+		setGains(kV, kA, 0, 0, 0, kDP);
 		this.target = target;
 		this.lMotor = lMotor;
 		this.rMotor = rMotor;
@@ -150,14 +151,15 @@ public class TankDriveFollower extends Follower<TankDriveMoment> {
 	 * @param kV       The velocity feedforward
 	 * @param kA       The acceleration feedforward
 	 * @param kP       The proportional gain
+	 * @param kI       The integral gain
 	 * @param kD       The derivative gain
 	 * @param kDP      The directional-proportional gain; for more information, see
 	 *                 {@link #setDP(double)}
 	 */
 	public TankDriveFollower(Followable<TankDriveMoment> target, Motor lMotor, Motor rMotor, PositionSource lDistSrc,
 			PositionSource rDistSrc, TimestampSource timer, DirectionSource dirSrc, double kV, double kA, double kP,
-			double kD, double kDP) {
-		setGains(kV, kA, kP, kD, kDP);
+			double kI, double kD, double kDP) {
+		setGains(kV, kA, kP, kI, kD, kDP);
 		this.target = target;
 		this.lMotor = lMotor;
 		this.rMotor = rMotor;

src/test/java/com/arctos6135/robotpathfinder/tests/follower/DynamicFollowerTest.java
@@ -11,7 +11,6 @@ import com.arctos6135.robotpathfinder.core.trajectory.TankDriveMoment;
 import com.arctos6135.robotpathfinder.follower.DynamicFollowable;
 import com.arctos6135.robotpathfinder.follower.DynamicFollower;
 import com.arctos6135.robotpathfinder.follower.DynamicTankDriveFollower;
-import com.arctos6135.robotpathfinder.follower.Follower;
 import com.arctos6135.robotpathfinder.math.MathUtils;
 import com.arctos6135.robotpathfinder.motionprofile.followable.profiles.TrapezoidalTankDriveProfile;
 import com.arctos6135.robotpathfinder.tests.TestHelper;
@@ -85,7 +84,7 @@ public class DynamicFollowerTest {
         FakeGyro gyro = new FakeGyro();
 
         DynamicFollower<TankDriveMoment> follower = new DynamicTankDriveFollower(followable, motor, motor, encoder,
-                encoder, timer, gyro, 0, 0, 0, 0, 0, updateDelay);
+                encoder, timer, gyro, 0, 0, 0, 0, 0, 0, updateDelay);
 
         follower.initialize();
         follower.run();
@@ -137,6 +136,7 @@ public class DynamicFollowerTest {
         double distance = helper.getDouble("distance", 1000);
         double kP = helper.getDouble("kP", MathUtils.getFloatCompareThreshold(), 1000);
         double kD = helper.getDouble("kD", MathUtils.getFloatCompareThreshold(), 1000);
+        double kI = helper.getDouble("kI", MathUtils.getFloatCompareThreshold(), 1000);
         double kV = helper.getDouble("kV", MathUtils.getFloatCompareThreshold(), 1000);
         double kA = helper.getDouble("kA", MathUtils.getFloatCompareThreshold(), 1000);
 
@@ -148,8 +148,8 @@ public class DynamicFollowerTest {
         FakeTimer timer = new FakeTimer();
         FakeMotor motor = new FakeMotor();
         FakeEncoder encoder = new FakeEncoder();
-        Follower<TankDriveMoment> follower = new DynamicTankDriveFollower(profile, motor, motor, encoder, encoder,
-                timer, kV, kA, kP, kD, profile.totalTime() * 2);
+        DynamicTankDriveFollower follower = new DynamicTankDriveFollower(profile, motor, motor, encoder, encoder,
+                timer, kV, kA, kP, kI, kD, profile.totalTime() * 2);
 
         follower.initialize();
         timer.value = profile.totalTime();
@@ -157,7 +157,7 @@ public class DynamicFollowerTest {
         assertThat(motor.value, not(closeTo(0.0, MathUtils.getFloatCompareThreshold())));
 
         follower.stop();
-        follower.setGains(1, 0, kP, 0, 0);
+        follower.setGains(1, 0, kP, 0, 0, 0);
         timer.value = 0;
         follower.initialize();
         timer.value = checkTime;
@@ -167,7 +167,7 @@ public class DynamicFollowerTest {
                 closeTo(profile.get(checkTime).getLeftVelocity(), MathUtils.getFloatCompareThreshold()));
 
         follower.stop();
-        follower.setGains(0, 1, kP, 0, 0);
+        follower.setGains(0, 1, kP, 0, 0, 0);
         timer.value = 0;
         encoder.value = 0;
         follower.initialize();

src/test/java/com/arctos6135/robotpathfinder/tests/follower/FollowerTest.java
@@ -1,8 +1,8 @@
 package com.arctos6135.robotpathfinder.tests.follower;
 
-import static org.hamcrest.Matchers.not;
-import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.closeTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertThat;
 
 import com.arctos6135.robotpathfinder.core.RobotSpecs;
@@ -126,7 +126,7 @@ public class FollowerTest {
 
         FakeTimer timer = new FakeTimer();
         Follower<TankDriveMoment> follower = new TankDriveFollower(traj, new FakeMotor(), new FakeMotor(),
-                new FakeEncoder(), new FakeEncoder(), timer, new FakeGyro(), 0, 0, 0, 0, 0);
+                new FakeEncoder(), new FakeEncoder(), timer, new FakeGyro(), 0, 0, 0, 0, 0, 0);
         // Assertion: Follower is not finished or running initially
         assertThat(follower.isFinished(), is(false));
         assertThat(follower.isRunning(), is(false));
@@ -179,6 +179,7 @@ public class FollowerTest {
         double maxA = helper.getDouble("maxA", 1.0);
         double distance = helper.getDouble("distance", 1000);
         double kP = helper.getDouble("kP", MathUtils.getFloatCompareThreshold(), 1000);
+        double kI = helper.getDouble("kI", MathUtils.getFloatCompareThreshold(), 1000);
         double kD = helper.getDouble("kD", MathUtils.getFloatCompareThreshold(), 1000);
         double kV = helper.getDouble("kV", MathUtils.getFloatCompareThreshold(), 1000);
         double kA = helper.getDouble("kA", MathUtils.getFloatCompareThreshold(), 1000);
@@ -191,8 +192,8 @@ public class FollowerTest {
         FakeTimer timer = new FakeTimer();
         FakeMotor motor = new FakeMotor();
         FakeEncoder encoder = new FakeEncoder();
-        Follower<TankDriveMoment> follower = new TankDriveFollower(profile, motor, motor, encoder, encoder, timer, kV,
-                kA, kP, kD);
+        TankDriveFollower follower = new TankDriveFollower(profile, motor, motor, encoder, encoder, timer, kV,
+                kA, kP, kI, kD);
 
         follower.initialize();
         timer.value = profile.totalTime();
@@ -200,7 +201,7 @@ public class FollowerTest {
         assertThat(motor.value, not(closeTo(0.0, MathUtils.getFloatCompareThreshold())));
 
         follower.stop();
-        follower.setGains(1, 0, kP, 0, 0);
+        follower.setGains(1, 0, kP, 0, 0, 0);
         timer.value = 0;
         follower.initialize();
         timer.value = checkTime;
@@ -210,7 +211,7 @@ public class FollowerTest {
                 closeTo(profile.get(checkTime).getLeftVelocity(), MathUtils.getFloatCompareThreshold()));
 
         follower.stop();
-        follower.setGains(0, 1, kP, 0, 0);
+        follower.setGains(0, 1, kP, 0, 0, 0);
         timer.value = 0;
         encoder.value = 0;
         follower.initialize();